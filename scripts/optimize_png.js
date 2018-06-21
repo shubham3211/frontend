046ab95731e077def3e84c37b69ca73f3b2b595f
François Hodierne <francois@hodierne.net>
2018-06-21 13:40:29 +0200

    optimization: optimize pngs with lossless compression

--- scripts/optimize_png.js
+++ scripts/optimize_png.js
@@ -1,8 +1,9 @@
+const path = require('path');
 const imagemin = require('imagemin');
 const imageminOptipng = require('imagemin-optipng');
 const imageminZopfli = require('imagemin-zopfli');
 
-const paths = [
+const filePaths = [
   'src/public/images/',
   'src/public/images/icons/',
   'src/public/images/collectives/',
@@ -12,6 +13,8 @@ const paths = [
   'src/static/images/buttons/',
   'src/static/images/email/',
   'src/static/images/icons/',
+  // 'src/public/images/mime-pdf' // crash with transparent
+  // 'src/public/images/icons/approved' // crash with transparent
 ];
 
 const options = {};
@@ -24,10 +27,12 @@ options.use.push(imageminOptipng({
 
 options.use.push(imageminZopfli({
   more: true,
+  transparent: true,
 }));
 
-paths.forEach(path => {
-  imagemin([`${path}*.png`], path, options).then((files) => {
-    console.log(`${path}*.png was optimized, ${files.length} files`);
-  });
+filePaths.forEach(filePath => {
+  imagemin([`${filePath}*.png`], path.dirname(`${filePath}*.png`), options)
+    .then(files => {
+      console.log(`${filePath}*.png was optimized, ${files.length} files`);
+    });
 });
